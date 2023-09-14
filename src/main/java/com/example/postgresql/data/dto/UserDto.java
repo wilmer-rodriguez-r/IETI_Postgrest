84985f7e5f04b98a3946f2b272e815defa396b48
Wilmer Rodriguez <wilmer.rodriguez-r@mail.escuelaing.edu.co>
2023-09-13 20:00:26 -0500

    laboratorio postgres

---
+++ src/main/java/com/example/postgresql/data/dto/UserDto.java
@@ -0,0 +1,33 @@
+package com.example.postgresql.data.dto;
+
+public class UserDto {
+
+    private String name;
+
+    private String email;
+
+    public UserDto(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDto() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
